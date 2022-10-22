5e8551037da91b5f310c60336b24529e089023b7
esrasumer <esrasumer28@gmail.com>
2022-10-22 14:42:59 +0300

    surname added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let surname = 'Sumer'
   return (
     <div className="App">
      İsa
      <h1>sumer</h1>
+     <h2>Bende surname ekledim {surname}</h2>
     </div>
   );
 }
