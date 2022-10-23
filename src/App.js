860e3e686d67a1735cb388b5ee611595e6e4b710
esrasumer <esrasumer28@gmail.com>
2022-10-23 23:33:14 +0300

    implement data fetching

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import Advice from "./components/Advice";
-import Title from "./components/Title";
-import axios from 'axios';
-import { useEffect, useState } from "react";
 import './App.css';
+import useFetchData from "./useFetchData";
+import { BsFillDice5Fill } from 'react-icons/bs';
+import { AiOutlineLoading3Quarters } from 'react-icons/ai';
 
-function App() {
-  const [advice,setAdvice] = useState("");
+
+export default function App() {
+
+  const { text, fetchAdvice, loading } = useFetchData()  //destruction
+
   return (
     <div className="card">
-      <Title />
-      <Advice />
+      <div className='advice'>
+        <h1 className='title'>Advice #{text?.id}</h1>
+        {loading ? <AiOutlineLoading3Quarters />
+          : <p>{text.advice}</p>
+        }
+        <div className='line' />
+        <button className='btn' onClick={fetchAdvice}>
+          <BsFillDice5Fill className='dice' />
+        </button>
+      </div>
     </div>
   );
 }
 
-export default App;
